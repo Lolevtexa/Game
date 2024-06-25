c5a0ec16f971672427c984ece8b803fe91d2d705
Lolevtexa <ceba.kek@gmail.com>
2024-06-25 16:28:22 +0300

    refactor: Update main.cpp to handle window resizing with SFML

--- source/main.cpp
+++ source/main.cpp
@@ -1,3 +1,4 @@
+#include "SFML/Graphics/View.hpp"
 #include "mainScene.hpp"
 #include <SFML/Graphics.hpp>
 
@@ -12,6 +13,11 @@ int main() {
     while (window.pollEvent(event)) {
       if (event.type == sf::Event::Closed)
         window.close();
+
+      if (event.type == sf::Event::Resized) {
+        sf::FloatRect visibleArea(0, 0, event.size.width, event.size.height);
+        window.setView(sf::View(visibleArea));
+      }
 
       mainScene.eventProcessing(event);
     }
